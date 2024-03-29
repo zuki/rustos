dd05fa5d5083f1ac51f48aef38a07c66627be6f8
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-03-29 12:37:03 +0900

    lab2のStackVecを実装

--- lib/stack-vec/src/lib.rs
+++ lib/stack-vec/src/lib.rs
@@ -3,18 +3,21 @@
 #[cfg(test)]
 mod tests;
 
-use core::slice;
+//use core::slice;
 use core::iter::IntoIterator;
 use core::ops::{Deref, DerefMut};
+use core::slice::{Iter, IterMut};
+//use core::option::IntoIter;
 
-/// A contiguous array type backed by a slice.
+/// スライスを背後に持つ連続配列型
 ///
-/// `StackVec`'s functionality is similar to that of `std::Vec`. You can `push`
-/// and `pop` and iterate over the vector. Unlike `Vec`, however, `StackVec`
-/// requires no memory allocation as it is backed by a user-supplied slice. As a
-/// result, `StackVec`'s capacity is _bounded_ by the user-supplied slice. This
-/// results in `push` being fallible: if `push` is called when the vector is
-/// full, an `Err` is returned.
+/// `StackVec`の機能は`std::Vec`に似ている。`push`と`pop`ができ、
+/// ベクトル全体にわたってiterateすることができる。しかし、`Vec`とは
+/// 異なり`StackVec`はメモリ割り当てが不要である。ユーザが指定した
+/// スライスを愛護に持つからである。そのため、`StackVec`の容量は
+/// ユーザが提供するスライスに _束縛_ される。この結果、`push` は
+/// 失敗する可能性がある。`push`が呼ばれた際にベクタがfullの場合は
+/// `Err`が返される。
 #[derive(Debug)]
 pub struct StackVec<'a, T: 'a> {
     storage: &'a mut [T],
@@ -22,69 +25,76 @@ pub struct StackVec<'a, T: 'a> {
 }
 
 impl<'a, T: 'a> StackVec<'a, T> {
-    /// Constructs a new, empty `StackVec<T>` using `storage` as the backing
-    /// store. The returned `StackVec` will be able to hold `storage.len()`
-    /// values.
+    /// `storage`をバッキングストアとして新しく空の`StackVec<T>`構築する。
+    /// 返される`StackVec`は`storage.len()`個の値を保持できる。
     pub fn new(storage: &'a mut [T]) -> StackVec<'a, T> {
-        unimplemented!()
+        StackVec::with_len(storage, 0)
     }
 
-    /// Constructs a new `StackVec<T>` using `storage` as the backing store. The
-    /// first `len` elements of `storage` are treated as if they were `push`ed
-    /// onto `self.` The returned `StackVec` will be able to hold a total of
-    /// `storage.len()` values.
+    /// `storage`をバッキングストアとして新しい `StackVec<T>` を
+    /// 作成する。storage`の最初の `len` 要素は `self.` に `push`
+    /// されたものとして扱われる。戻り値の `StackVec` は合計
+    /// `storage.len()` 個の値を保持できる。
     ///
     /// # Panics
     ///
     /// Panics if `len > storage.len()`.
     pub fn with_len(storage: &'a mut [T], len: usize) -> StackVec<'a, T> {
-        unimplemented!()
+        if len > storage.len() {
+            panic!("len > storage.len()");
+        }
+
+        StackVec { storage, len }
     }
 
-    /// Returns the number of elements this vector can hold.
+    /// このベクタが保持できる要素の数を返す.
     pub fn capacity(&self) -> usize {
-        unimplemented!()
+        self.storage.len()
     }
 
-    /// Shortens the vector, keeping the first `len` elements. If `len` is
-    /// greater than the vector's current length, this has no effect. Note that
-    /// this method has no effect on the capacity of the vector.
+    /// 先頭から`len`個の要素を残してベクトルを短くする。
+    /// `len`がベクタの現在の長さより大きい場合、このメソッドは
+    /// 何もしない。このメソッドはベクトルの容量には影響しない
+    /// ことに注意。
     pub fn truncate(&mut self, len: usize) {
-        unimplemented!()
+        if len > self.len {
+            return;
+        }
+        self.len = len;
     }
 
-    /// Extracts a slice containing the entire vector, consuming `self`.
+    /// `self`を消費してベクトル全体を含むスライスを取り出す。
     ///
-    /// Note that the returned slice's length will be the length of this vector,
-    /// _not_ the length of the original backing storage.
+    /// 返されるスライスの長さはこのベクトルの長さであり、元の
+    /// ストレージの長さでは_ない_ことに注意
     pub fn into_slice(self) -> &'a mut [T] {
-        unimplemented!()
+        &mut self.storage[0..self.len]
     }
 
     /// Extracts a slice containing the entire vector.
     pub fn as_slice(&self) -> &[T] {
-        unimplemented!()
+        &self.storage[0..self.len]
     }
 
     /// Extracts a mutable slice of the entire vector.
     pub fn as_mut_slice(&mut self) -> &mut [T] {
-        unimplemented!()
+        &mut self.storage[0..self.len]
     }
 
     /// Returns the number of elements in the vector, also referred to as its
     /// 'length'.
     pub fn len(&self) -> usize {
-        unimplemented!()
+        self.len
     }
 
     /// Returns true if the vector contains no elements.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.len() == 0
     }
 
     /// Returns true if the vector is at capacity.
     pub fn is_full(&self) -> bool {
-        unimplemented!()
+        self.len() == self.capacity()
     }
 
     /// Appends `value` to the back of this vector if the vector is not full.
@@ -94,17 +104,60 @@ impl<'a, T: 'a> StackVec<'a, T> {
     /// If this vector is full, an `Err` is returned. Otherwise, `Ok` is
     /// returned.
     pub fn push(&mut self, value: T) -> Result<(), ()> {
-        unimplemented!()
+        if self.is_full() {
+            return Err(());
+        }
+
+        self.storage[self.len] = value;
+        self.len += 1;
+        Ok(())
     }
 }
 
 impl<'a, T: Clone + 'a> StackVec<'a, T> {
-    /// If this vector is not empty, removes the last element from this vector
-    /// by cloning it and returns it. Otherwise returns `None`.
+    /// このベクタが空でない場合、このベクタから最後の要素を
+    /// クローンして取り除き、それを返す。そうでない場合は
+    /// `None` を返す。
     pub fn pop(&mut self) -> Option<T> {
-        unimplemented!()
+        if self.is_empty() {
+            return None;
+        }
+        self.len -= 1;
+        Some(self.storage[self.len].clone())
     }
 }
 
 // FIXME: Implement `Deref`, `DerefMut`, and `IntoIterator` for `StackVec`.
+
+impl<'a, T: 'a> Deref for StackVec<'a, T> {
+    type Target = [T];
+
+    fn deref(&self) -> &Self::Target {
+        &self.storage[0..self.len]
+    }
+}
+
+impl<'a, T: 'a> DerefMut for StackVec<'a, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.storage
+    }
+}
+
+impl<'a, T: 'a> IntoIterator for StackVec<'a, T> {
+    type Item = &'a mut T;
+    type IntoIter = IterMut<'a, T>;
+
+    fn into_iter(self) -> IterMut<'a, T> {
+        self.into_slice().into_iter()
+    }
+}
+
 // FIXME: Implement IntoIterator` for `&StackVec`.
+impl<'a, T: 'a> IntoIterator for &'a StackVec<'a, T> {
+    type Item = &'a T;
+    type IntoIter = Iter<'a, T>;
+
+    fn into_iter(self) -> Iter<'a, T> {
+        self.as_slice().iter()
+    }
+}
