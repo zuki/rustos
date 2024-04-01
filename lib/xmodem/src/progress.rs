ef88dcabe207468be0a18d099927aa85d2047052
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-04-01 11:59:55 +0900

    コメントを翻訳

--- lib/xmodem/src/progress.rs
+++ lib/xmodem/src/progress.rs
@@ -1,23 +1,23 @@
-/// Enum representing how much progress has been made transmitting/receiving.
+/// 送受信の進捗状況を表す列挙型.
 ///
-/// A value of this type is passed in to the progress callback supplied to
-/// methods like [`Xmodem::transmit_with_progress()`],
-/// [`Xmodem::receive_with_progress()`], and [`Xmodem::new_with_progress()`]. It
-/// is intended to be used by progress indicators or for debugging purposes.
+/// この型の値は[`Xmodem::transmit_with_progress()`],
+/// [`Xmodem::receive_with_progress()`], [`Xmodem::new_with_progress()`]
+/// などのメソッドに渡される進捗コールバックに渡される。
+/// これは進捗インジケータやデバッグ目的で使用することを意図している。
 #[derive(Debug, Copy, Clone)]
 pub enum Progress {
-    /// Waiting for receiver to send NAK.
+    /// NAKを送信する受信者を待っている.
     Waiting,
-    /// Download/upload has started.
+    /// ダウンロード/アップロードが開始された.
     Started,
-    /// Packet `.0` was transmitted/received.
+    /// パケット `.0` が送信/受信された.
     Packet(u8),
     NAK,
     Unknown,
 }
 
-/// Type for progress callbacks.
+/// 進捗コールバックの型.
 pub type ProgressFn = fn(Progress);
 
-/// Noop progress callback.
+/// Noop進捗コールバック.
 pub fn noop(_: Progress) {  }
