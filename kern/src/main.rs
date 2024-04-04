012a1dec761b9fc78de112358afc1cd3c23b5fff
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-04-04 10:09:25 +0900

    lib/timerのテストを実施

--- kern/src/main.rs
+++ kern/src/main.rs
@@ -18,8 +18,25 @@ use console::kprintln;
 
 // FIXME: You need to add dependencies here to
 // test your drivers (Phase 2). Add them as needed.
+use pi::timer;
+use core::time::Duration;
 
-fn kmain() -> ! {
+const GPIO_BASE: usize = 0x3F000000 + 0x200000;
+
+const GPIO_FSEL1: *mut u32 = (GPIO_BASE + 0x04) as *mut u32;
+const GPIO_SET0: *mut u32 = (GPIO_BASE + 0x1C) as *mut u32;
+const GPIO_CLR0: *mut u32 = (GPIO_BASE + 0x28) as *mut u32;
+
+unsafe fn kmain() -> ! {
     // FIXME: Start the shell.
-    unimplemented!()
+    let fsel = (GPIO_FSEL1.read_volatile() & !(0x7 << 18)) | (1 << 18);
+    GPIO_FSEL1.write_volatile(fsel);
+    // FIXME: STEP 2: Continuously set and clear GPIO 16.
+    loop {
+        GPIO_SET0.write_volatile(1 << 16);
+        pi::timer::spin_sleep(Duration::from_millis(1000));
+        GPIO_CLR0.write_volatile(1 << 16);
+        pi::timer::spin_sleep(Duration::from_millis(1000));
+    }
+
 }
