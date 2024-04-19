21f011adba307292545eabda896bf0ede5aebd8a
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-04-19 11:42:49 +0900

    align_down/upの実装

--- kern/src/allocator/util.rs
+++ kern/src/allocator/util.rs
@@ -1,22 +1,36 @@
-/// Align `addr` downwards to the nearest multiple of `align`.
+fn is_power2(value: usize) -> bool {
+    if value == 0 {
+        return false;
+    }
+    value & (value - 1) == 0
+}
+/// `addr` を `align` の最も近い倍数の下方向にアラインする.
 ///
-/// The returned usize is always <= `addr.`
+/// 返される`usize`は常に <= `addr.`
 ///
 /// # Panics
 ///
-/// Panics if `align` is not a power of 2.
+/// `align` は2のべき乗でない場合はパニック.
 pub fn align_down(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    if !is_power2(align) {
+        panic!("not a power of 2");
+    }
+    (addr  / align) * align
 }
 
-/// Align `addr` upwards to the nearest multiple of `align`.
+/// `addr` を `align` の最も近い倍数の上方向にアラインする.
 ///
-/// The returned `usize` is always >= `addr.`
+/// 返される`usize`は常に >= `addr.`
 ///
 /// # Panics
 ///
-/// Panics if `align` is not a power of 2
-/// or aligning up overflows the address.
+/// `align` は2のべき乗でない、または、アライメントでアドレスが
+/// オーバーフローする場合はパニック.
 pub fn align_up(addr: usize, align: usize) -> usize {
-    unimplemented!()
+    if !is_power2(align) {
+        panic!("not a power of 2");
+    } else if addr + align > std::usize::MAX {
+        panic!("aligning up overflows the address")
+    }
+    ((addr + align - 1) / align) * align
 }
