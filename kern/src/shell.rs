d4450c7924ae4c186c2059efd64b06d56c8db761
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-05-03 17:30:49 +0900

    cwd, cd, lsを実装

--- kern/src/shell.rs
+++ kern/src/shell.rs
@@ -1,5 +1,7 @@
 use shim::io;
 use shim::path::{Path, PathBuf};
+use shim::ffi::{OsStr, OsString};
+use alloc::string::String;
 
 use stack_vec::StackVec;
 use core::str;
@@ -7,11 +9,14 @@ use core::str;
 use pi::atags::Atags;
 
 use fat32::traits::FileSystem;
-use fat32::traits::{Dir, Entry};
+use fat32::traits::{Dir, Entry, File};
+use fat32::traits::metadata::{Metadata, Timestamp};
+use crate::fs::PiVFatHandle;
 
 use crate::console::{kprint, kprintln, CONSOLE};
 use crate::ALLOCATOR;
 use crate::FILESYSTEM;
+
 
 /// `Command`のパースに失敗した際のエラー型.
 #[derive(Debug)]
@@ -52,11 +57,70 @@ impl<'a> Command<'a> {
     }
 }
 
+fn canonicalize(cwd: &PathBuf) -> PathBuf {
+    let mut path = PathBuf::new();
+    for c in cwd.iter() {
+        match c.to_str() {
+            Some(".") => (),
+            Some("..") => {
+                let _ = path.pop();
+            }
+            Some(c) => path.push(c),
+            None => (),
+        }
+    }
+    path
+}
+
+fn print_ls_entry(entry: fat32::vfat::Entry<PiVFatHandle>, show_all: bool)  {
+    if !show_all && (entry.metadata().attributes.hidden() || entry.name() == "." || entry.name() == "..") {
+        return;
+    }
+
+    let mut line = String::new();
+    if entry.is_dir() {
+        line.push('d');
+    } else {
+        line.push('-');
+    }
+
+    if entry.metadata().hidden() {
+        line.push('h');
+    } else {
+        line.push('-');
+    }
+    if entry.metadata().attributes.read_only() {
+        line.push('r');
+    } else {
+        line.push('-');
+    }
+
+    let size = if entry.is_file() { entry.as_file().unwrap().size() } else { 0_u64 };
+
+    kprintln!("{} {:>10} {} {}", line, size, entry.metadata().modified(), entry.name());
+}
+
+fn do_ls(cwd: &PathBuf, show_all: bool)  {
+    let path = canonicalize(cwd);
+    if let Ok(entry) = FILESYSTEM.open(path) {
+        if entry.is_dir() {
+            for e in entry.as_dir().unwrap().entries().unwrap() {
+               print_ls_entry(e, show_all);
+            }
+        } else {
+            print_ls_entry(entry, show_all);
+        }
+    } else {
+        kprintln!("invalid path: {}", canonicalize(cwd).display());
+    }
+}
+
 /// 各行のプリフィックスとして`prefix`を使ってシェルを開始する。
 /// `exit`コマンドが呼び出されたらこの関数はリターンする。
 pub fn shell(prefix: &str) -> () {
     let mut lbuf = [0u8; 512];
     let mut line = StackVec::new(&mut lbuf);
+    let mut cwd = PathBuf::from("/");
 
     kprint!("{}", prefix);
     loop {
@@ -75,6 +139,43 @@ pub fn shell(prefix: &str) -> () {
                                     kprint!("{} ", &command.args[i]);
                                 }
                                 kprint!("\n");
+                            }
+                            &"cwd" => {
+                                kprintln!("\n{}", cwd.display());
+                            }
+                            &"cd" => {
+                                kprint!("\n");
+                                if command.args.len() != 2 {
+                                    kprintln!("cd requires <directory>");
+                                } else {
+                                    cwd.push(command.args[1]);
+                                    cwd = canonicalize(&cwd);
+                                }
+                            }
+                            &"ls" => {
+                                kprint!("\n");
+                                match command.args.len() {
+                                    3 => {
+                                        if command.args[1] != "-a" {
+                                            kprintln!("bad arguments");
+                                        } else {
+                                            let dir = &cwd.join(PathBuf::from(command.args[2]));
+                                            do_ls(&dir, true);
+                                        }
+                                    }
+                                    2 => {
+                                        if command.args[1] ==  "-a" {
+                                            do_ls(&cwd, true);
+                                        } else {
+                                            let dir = &cwd.join(PathBuf::from(command.args[1]));
+                                            do_ls(&dir, false);
+                                        }
+                                    }
+                                    1 => {
+                                        do_ls(&cwd, false);
+                                    }
+                                    _ => kprintln!("too many args"),
+                                }
                             }
                             &"exit" => {
                                 kprintln!("\nexit");
