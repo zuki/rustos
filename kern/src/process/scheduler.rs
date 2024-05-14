cb20d7890b8b7b018a153abed870f48c89f9eae2
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-05-14 18:05:09 +0900

    実行すべきプロセスがない場合、スケジューラはwfiで待機するように変更

--- kern/src/process/scheduler.rs
+++ kern/src/process/scheduler.rs
@@ -58,7 +58,8 @@ impl GlobalScheduler {
             if let Some(id) = rtn {
                 return id;
             }
-            aarch64::wfe();
+            //aarch64::wfe();
+            aarch64::wfi();
         }
     }
 
