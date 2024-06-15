9dd58883666a1c21a6e43786b52697f2e952a2ac
SUZUKI Keiji <zuki.ebetsu@gmail.com>
2024-06-15 17:54:10 +0900

    lab5, p3を実装.未テスト

--- user/echo/src/main.rs
+++ user/echo/src/main.rs
@@ -19,5 +19,26 @@ fn main() {
 
 fn main_inner() -> OsResult<!> {
     // Lab 5 3
-    unimplemented!("main_inner()")
+    let descriptor = sock_create();
+    sock_listen(descriptor, 80_u16);
+    print!("connect ");
+    loop {
+        let status = sock_status(descriptor)?;
+        if status.can_send {
+            println!(" ok.");
+            break;
+        } else {
+            print!(".");
+            sleep(Duration::from_secs(1));
+        }
+    }
+    let welcome: &str = "welcome echo server!\n";
+    sock_send(descriptor, welcome.as_bytes());
+
+    let mut buf = [0_u8; 512];
+    loop {
+        let size = sock_recv(descriptor, &mut buf)?;
+        println!("{:?}", &buf[..size]);
+        let _ = sock_send(descriptor, &buf[..size])?;
+    }
 }
